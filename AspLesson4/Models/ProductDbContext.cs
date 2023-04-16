f97db82dc0d3e3f8ed4a16dfc16bc8205164c472
camalzadeelvin <camalzadeelvin@STHQ012D-01>
2023-04-16 16:16:33 +0400

    Add project files.

---
+++ AspLesson4/Models/ProductDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AspLesson4.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspLesson4.Models
+{
+    public class ProductDbContext:DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> opt)
+            :base(opt)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
